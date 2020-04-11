0eb7a246058a37e54c4230b61a2cfbaa653f4712
Dublin Anondson <stratophone2112@gmail.com>
2020-04-11 14:53:50 -0500

    feat: added the base porfolio page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React from "react"
 import Footer from "../components/layouts/subcomponents/footer-section"
 import SEO from "../components/seo"
+
 import "../components/styles.css"
 import ContactSection from "../components/layouts/subcomponents/contact-section"
 import AboutSection from "../components/layouts/subcomponents/about-section"
 import TitleSection from "../components/layouts/subcomponents/title-section"
+import Navbar from "../components/layouts/subcomponents/navbar/navbar"
 
 const IndexPage = () => (
   <>
     <SEO title="Home" />
+    <Navbar />
     <TitleSection />
     <AboutSection />
     <ContactSection />
---
+++ src/pages/portfolio.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Footer from "../components/layouts/subcomponents/footer-section"
+import SEO from "../components/seo"
+import "../components/styles.css"
+import Navbar from "../components/layouts/subcomponents/navbar/navbar"
+
+const PortfolioPage = () => (
+  <>
+    <SEO title="Portfolio" />
+    <Navbar />
+    <Footer />
+  </>
+)
+
+export default PortfolioPage
