b59d1ed2d79b265f7e743492063996252e589773
Dublin Anondson <stratophone2112@gmail.com>
2020-10-25 18:15:34 -0500

    test: added tests for external link and post link components.

---
+++ src/components/external-link/external-link.test.js
@@ -0,0 +1,14 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import ExternalLink from "./external-link"
+
+describe("External Link Snapshot test", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(
+        <ExternalLink url="http://localhost:8080"> LocalHost </ExternalLink>
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
